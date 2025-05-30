f5de3622564b4f79b6c3bba47a9fab7263d30a73
paulnguyendesigns <paulxnguyen12@gmail.com>
2025-05-30 14:42:03 -0700

    change background

--- dahacks3.5/src/App.js
+++ dahacks3.5/src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="website">
-      <header className="website-header">
-      </header>
+      <header className="website-header"></header>
+
+      <img src="website-pic1.png" alt="input background" className="input-bg" />
+
       <div className="input-container">
         <input className="exchange-button" type="text" placeholder="USD to EUR" />
+        <img src="arrow.png" alt="arrow"/>
         <input className="exchange-button" type="text" placeholder="EUR to USD" />
       </div>
     </div>
@@ -14,3 +17,5 @@ function App() {
 }
 
 export default App;
+
+
